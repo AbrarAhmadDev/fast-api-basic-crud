68ac373e20a197a738b5567d8eee996a49ae75d2
Abrar Ahmad <abrarahmad.workplace@gmail.com>
2026-08-12 02:45:14 +0500

    basic pagination, no counts

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from contextlib import asynccontextmanager
 from datetime import datetime, timezone
-from fastapi import FastAPI, HTTPException, Depends
-from typing import Any, Annotated, Generic, TypeVar
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from fastapi import FastAPI, HTTPException, Depends, Query, Request
+from typing import Any, Annotated, Generic, Optional, TypeVar
+from sqlmodel import Field, Session, SQLModel, create_engine, func, select
 
 from pydantic import BaseModel
 
@@ -69,10 +69,44 @@ T = TypeVar("T")
 class Response(BaseModel, Generic[T]):
     data: T
 
-@app.get("/campaigns", response_model=Response[list[Campaign]])
-async def read_campaigns(session: SessionDep):
-    data = session.exec(select(Campaign)).all()
-    return {"data": data}
+class PaginatedResponse(BaseModel, Generic[T]):
+    data: T
+    next: Optional[str]
+    prev: Optional[str]
+    # count: int
+    
+
+@app.get("/campaigns", response_model=PaginatedResponse[list[Campaign]])
+async def read_campaigns(request : Request, session: SessionDep, page: int = Query(1,ge=1), page_size : int = Query(3,ge=1)): # type: ignore
+    limit = page_size
+    offset = (page-1)*limit
+    data = session.exec(select(Campaign).order_by(Campaign.campaign_id).offset(offset).limit(limit)).all() #type:ignore
+
+    base_url = str(request.url).split("?")[0]
+
+    # Approach 1
+
+    # total = session.exec(select(func.count()).select_from(Campaign)).one()
+
+    # if offset + limit < total:
+    #     next_url = f"{base_url}?page={page+1}&page_size={limit}"
+    # else:
+    #     next_url = None
+    
+    # Approach 2
+    next_url = f"{base_url}?page={page+1}&page_size={limit}"
+
+    if page > 1:
+        prev_url = f"{base_url}?page={page-1}&page_size={limit}"
+    else:
+        prev_url = None
+    
+    return {
+        # "count":total,
+        "next": next_url,
+        "prev": prev_url,
+        "data": data
+        } # type: ignore
 
 @app.get("/campaigns/{id}",response_model=Response[Campaign])
 async def read_campaign(id : int, session: SessionDep):
